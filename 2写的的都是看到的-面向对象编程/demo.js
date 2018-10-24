f03324dfdcf2fb27d0deae417d37f10be05037d7
D1n910 <wyd1n910@gmail.com>
2018-10-24 14:04:13 +0800

    闭包内部实现一个完整的类

--- 2写的的都是看到的-面向对象编程/demo.js
+++ 2写的的都是看到的-面向对象编程/demo.js
@@ -127,3 +127,44 @@ for (var i = 0;i<=10;i++) {
     },10)
   })(i)
 }
+
+// 有时候在闭包内部实现一个完整的类然后将其返回
+var Book = (function() {
+  // 静态私有变量  
+  var bookNum = 99;
+  // 静态私有方法
+  function checkBook(name) {
+  }
+  
+  // 创建类
+  function _book(newId, newName, newPrice) {
+    // 私有变量
+    var name, price;
+    // 私有方法
+    function checkID(id){}
+    // 特权方法
+    this.getName = function() {};
+    this.getPrice = function() {};
+    this.setName = function() {};
+    this.setPrice = function() {};
+    // 公有属性
+    this.id = newId;
+    // 公有方法
+    this.copy = function() {};
+    bookNum++
+    if (bookNum > 100)
+      throw new Error('我们仅出版100本书。');
+      //构造器
+      this.setName(name);
+      this.setPrice(price);
+  }
+  // 构建原型
+  _book.prototype = {
+    // 静态共有属性
+    isJSBook: false,
+    // 静态公有方法
+    dispaly: function(){}
+  };
+  
+  return _book;
+})();
